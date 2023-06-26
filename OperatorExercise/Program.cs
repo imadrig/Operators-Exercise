dcac8d646fb1efe63d9dd34761f625fed3230f9a
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-06-25 21:35:32 -0400

    Initial Commit - Exercise 1 partially completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int remainder = a % b;
+
+            int sum = a + b;
+            Console.WriteLine($"The sum of 17 + 4 is {sum}.");
+
+            int difference = a - b;
+            Console.WriteLine($"The difference of 17 - 4 is {difference}.");
+
+            int product = a * b; 
+            Console.WriteLine($"The product of 17 x 4 is {product}.");
+
+
         }
     }
 }
