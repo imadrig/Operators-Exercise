5b422021ac7d9e228d0fac9180b661d0cbc28363
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-06-26 17:38:22 -0400

    Completed Exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
@@ -8,7 +10,7 @@
             int b = 4;
             
             
-
+            // Exercise 1
             int sum = a + b;
             Console.WriteLine($"The sum of 17 + 4 is {sum}.");
 
@@ -25,7 +27,24 @@
             Console.WriteLine($"The remainder of 17 / 4 is {remainder}.");
 
             if (a == 17 && b == 4);
-            Console.WriteLine($"17 / 4 is {quotient} remainder {remainder}.");
+            Console.WriteLine($"17 / 4 is {quotient} remainder {remainder}."
+                + Environment.NewLine
+                + Environment.NewLine);
+
+
+            // Exercise 2
+            Console.WriteLine("What is the radius of your circle ?");
+            var radius = double.Parse(Console.ReadLine());
+
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of your circle with a radius of {radius} is {area}");
+
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+         }
     }
 }
